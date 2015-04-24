15474c30bb541cfa33fff671edb2952047493ae9
Cinder <cinder@sdf.org>
2015-04-23 22:32:30 -0600

    Update zlib for darwin

--- build-cmd.sh
+++ build-cmd.sh
@@ -89,13 +89,13 @@ pushd "$PNG_SOURCE_DIR"
             # sdk=/Developer/SDKs/MacOSX10.6.sdk/
             # sdk=/Developer/SDKs/MacOSX10.7.sdk/
             # sdk=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk/
-            sdk=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.9.sdk/
+            sdk=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/
 
             # Keep min version back at 10.5 if you are using the
             # old llqtwebkit repo which builds on 10.5 systems.
             # At 10.6, libpng will start using __bzero() which doesn't
             # exist there.
-            opts="${TARGET_OPTS:--arch i386 -arch x86_64 -iwithsysroot $sdk -mmacosx-version-min=10.7}"
+            opts="${TARGET_OPTS:--arch i386 -arch x86_64 -iwithsysroot $sdk -mmacosx-version-min=10.8}"
 
             # Install name for dylibs (if we wanted to build them).
             # The outline of a dylib build is here disabled by '#dylib#' 
